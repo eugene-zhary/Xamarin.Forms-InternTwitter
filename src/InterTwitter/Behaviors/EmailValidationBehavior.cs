fcc3b0d1e88705c36e3e25917f71840de1f06171
Stas Kryvosheia <stask49@gmail.com>
2021-04-18 01:21:48 +0300

    - Added validation class for Name, Email and password - Added behaviors for Name and Email validations

---
+++ src/InterTwitter/Behaviors/EmailValidationBehavior.cs
@@ -0,0 +1,56 @@
+﻿using InterTwitter.Controls;
+using InterTwitter.Resources;
+using InterTwitter.Validators;
+using Xamarin.Forms;
+
+namespace InterTwitter.Behaviors
+{
+    class EmailValidationBehavior : Behavior<RegisteringEntry>
+    {
+        #region -- Overrides --
+
+        protected override void OnAttachedTo(RegisteringEntry bindable)
+        {
+            bindable.TextChanged += OnTextChanged;
+            base.OnAttachedTo(bindable);
+        }
+
+        protected override void OnDetachingFrom(RegisteringEntry bindable)
+        {
+            bindable.TextChanged -= OnTextChanged;
+            base.OnDetachingFrom(bindable);
+        }
+
+        #endregion
+
+        #region -- Private helpers --
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is RegisteringEntry registeringEntry)
+            {
+                var preparedInput = e.NewTextValue.Trim();
+
+                if (string.IsNullOrWhiteSpace(preparedInput))
+                {
+                    registeringEntry.IsErrorTextVisible = true;
+                    registeringEntry.UnderlineColor = Color.Red;
+                    registeringEntry.ErrorText = Strings.CannotBeBlank;
+                }
+                else if (!StringValidator.Validate(preparedInput, StringValidator.Email))
+                {
+                    registeringEntry.IsErrorTextVisible = true;
+                    registeringEntry.UnderlineColor = Color.Red;
+                    registeringEntry.ErrorText = Strings.EmailInputError;
+                }
+                else
+                {
+                    registeringEntry.IsErrorTextVisible = false;
+                    registeringEntry.UnderlineColor = Color.Black;
+                }
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/InterTwitter/Validators/StringValidator.cs
@@ -0,0 +1,58 @@
+﻿using System.Text.RegularExpressions;
+
+namespace InterTwitter.Validators
+{
+    public class StringValidator
+    {
+        #region --- Private Constants ---
+
+        // accepts only latin symbols
+        private const string USERNAME_REGEX =
+            @"^[A-Za-z]{1,}$";
+
+        private const string EMAIL_REGEX =
+            @"^[\w\.]+@([\w-]+\.)+[\w-]{1,}$";
+
+        // accepts string with at least one uppercase letter and one digit
+        private const string PASSWORD_REGEX =
+            @"^(?=.*\d)(?=.*[A-ZА-ЯЁ]).{6,}$";
+
+        #endregion
+
+        #region --- Properties ---
+
+        private string Pattern { get; }
+
+        public static StringValidator Name { get; }
+        public static StringValidator Email { get; }
+        public static StringValidator Password { get; }
+
+        #endregion
+
+        #region --- Constructors ---
+
+        private StringValidator(string pattern)
+        {
+            Pattern = pattern;
+        }
+
+        static StringValidator()
+        {
+            Email = new StringValidator(EMAIL_REGEX);
+            Password = new StringValidator(PASSWORD_REGEX);
+            Name = new StringValidator(USERNAME_REGEX);
+        }
+
+        #endregion
+
+        #region --- Public Methods ---
+
+        public static bool Validate(string input, StringValidator type)
+        {
+            return !string.IsNullOrEmpty(input) &&
+                   Regex.IsMatch(input, type.Pattern);
+        }
+
+        #endregion
+    }
+}
