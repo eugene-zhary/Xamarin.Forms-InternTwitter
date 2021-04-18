6dde732554d10025566cf3ae8c2b3e623380daa9
Stas Kryvosheia <stask49@gmail.com>
2021-04-19 02:52:01 +0300

    Added skeleton for SignIn page

---
+++ src/InterTwitter/ViewModels/SignInPageViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using InterTwitter.Helpers;
+using InterTwitter.Models;
+using InterTwitter.Resources;
+using InterTwitter.Services.UserService;
+using Prism.Navigation;
+using Prism.Services;
+
+namespace InterTwitter.ViewModels
+{
+    public class SignInPageViewModel : BaseViewModel
+    {
+        private readonly IPageDialogService _pageDialogService;
+        private readonly IUserService _userService;
+
+        public SignInPageViewModel(INavigationService navigationService,
+            IPageDialogService pageDialogService,
+            IUserService userService)
+            : base(navigationService)
+        {
+            _pageDialogService = pageDialogService;
+            _userService = userService;
+        }
+
+        #region -- Public Properties --
+
+        private string _email;
+        public string Email
+        {
+            get => _email;
+            set => SetProperty(ref _email, value);
+        }
+
+        private string _password;
+        public string Password
+        {
+            get => _password;
+            set => SetProperty(ref _password, value);
+        }
+
+        private ICommand _signInCommand;
+        public ICommand SignInCommand => _signInCommand ??= SingleExecutionCommand.FromFunc(OnSignIn);
+
+        #endregion
+
+        #region -- Overrides --
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            if (parameters.TryGetValue(nameof(User), out User user))
+            {
+                Email = user.Email;
+            }
+        }
+
+        #endregion
+
+        #region -- Private helpers --
+
+        private async Task OnSignIn()
+        {
+            if (!string.IsNullOrWhiteSpace(Email) &&
+                !string.IsNullOrEmpty(Password))
+            {
+                // todo : check user through service and if success then
+                // await NavigationService.NavigateAsync($"/{nameof(NavigationPage)}/{nameof(FlyoutNavigationView)}");
+            }
+            else
+            {
+                await _pageDialogService.DisplayAlertAsync(Strings.LogInErrorTitle, Strings.EmptyFieldsError,
+                    Strings.Ok);
+            }
+        }
+
+        #endregion
+    }
+}
