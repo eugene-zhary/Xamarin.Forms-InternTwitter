4e81e5244d0d547089fbb44bc062eb7751a9ea3a
DESKTOP-N25H0J4\User <eugenezhary05@gmail.com>
2021-04-19 12:07:35 +0300

    - android splashScreen added

--- src/InterTwitter.Android/MainActivity.cs
+++ src/InterTwitter.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Xamarin.Forms;
 
 namespace InterTwitter.Droid
 {
-    [Activity(Label = "InterTwitter", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "@string/ApplicationName")]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         #region -- Overrides --
---
+++ src/InterTwitter.Android/SplashActivity.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using AndroidX.AppCompat.App;
+using System.Threading.Tasks;
+
+namespace InterTwitter.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        async void SimulateStartup()
+        {
+            await Task.Delay(500);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
