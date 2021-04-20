676b3318e7affb7d3455db4be795e2db2f940940
eugene-zhary <eugenezhary05@gmail.com>
2021-04-20 11:49:14 +0300

    - disabled dark mode for ios
    Signed-off-by: eugene-zhary <eugenezhary05@gmail.com>

---
+++ src/InterTwitter.iOS/Controls/CustomNaviPageRenderer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using InterTwitter.iOS.Controls;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(NavigationPage), typeof(CustomNaviPageRenderer))]
+namespace InterTwitter.iOS.Controls
+{
+
+        public class CustomNaviPageRenderer : NavigationRenderer
+        {
+            protected override void OnElementChanged(VisualElementChangedEventArgs e)
+            {
+                base.OnElementChanged(e);
+
+                OverrideUserInterfaceStyle = UIUserInterfaceStyle.Light;
+            }
+        }
+    }
