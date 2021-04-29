d57e2a27a12c7ace2fbf0181eeb7790b1939ce7b
Stas Kryvosheia <stask49@gmail.com>
2021-04-30 02:11:29 +0300

    - Added DataTemplateSelector for notifications, finished main requirements

---
+++ src/InterTwitter/Resources/NotificationTemplates.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InterTwitter.Resources
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NotificationTemplates : ResourceDictionary
+    {
+        public NotificationTemplates()
+        {
+            InitializeComponent();
+        }
+    }
+}
